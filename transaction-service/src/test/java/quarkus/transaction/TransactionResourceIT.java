7e30115bfb869f65ea2c00f652ae1a6f28a6c2b7
sharofiddin <sharofiddin@pop-os.localdomain>
2023-01-02 03:15:02 +0500

    optional files files

---
+++ transaction-service/src/test/java/quarkus/transaction/TransactionResourceIT.java
@@ -0,0 +1,8 @@
+package quarkus.transaction;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class TransactionResourceIT extends TransactionResourceTest {
+    // Execute the same tests but in packaged mode.
+}
