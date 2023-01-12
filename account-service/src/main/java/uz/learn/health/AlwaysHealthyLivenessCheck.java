1efb0a1734d9a956f816e399532471ac3528b8dd
sharofiddin <sharofiddin@pop-os.localdomain>
2023-01-13 01:25:26 +0500

    health

---
+++ account-service/src/main/java/uz/learn/health/AlwaysHealthyLivenessCheck.java
@@ -0,0 +1,20 @@
+package uz.learn.health;
+
+import java.util.Date;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+public class AlwaysHealthyLivenessCheck implements HealthCheck {
+
+	@Override
+	public HealthCheckResponse call() {
+		
+		return HealthCheckResponse.named("Always live")
+				.withData("time", String.valueOf(new Date()))
+				.up().build();
+	}
+
+}
